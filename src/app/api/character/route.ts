cb4f372c611ce89f95c6479e013b18c6b0cf1c52
Yash <sharmayash380@gmail.com>
2024-02-16 02:27:20 +0530

    --

--- src/app/api/character/route.ts
+++ src/app/api/character/route.ts
@@ -3,7 +3,5 @@ import characters from "@/data/characters.json";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  console.log(characters.data);
-
   return NextResponse.json({ characters: characters.data });
 }
